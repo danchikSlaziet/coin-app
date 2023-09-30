6a4f28e82ee4c08a6e5a4db42810bd6acde6c6cc
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-09-30 18:35:37 +0300

    initial routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import CoinList from "./components/CoinList";
 import Hero from "./components/Hero"
 import Welcome from "./components/Welcome";
+import CoinInfo from "./components/CoinInfo";
 
 function App() {
   return (
     <main>
       <Hero />
       <Welcome />
-      <CoinList />
+      <Routes>
+        <Route path="/*" element={<CoinList />} />
+        <Route path='/:id' element={<CoinInfo />}/>
+      </Routes>
     </main>
   );
 };
--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,5 +1,7 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import examplePath from '../images/hero-bg.png';
 import {  ICoin } from '../types/coin';
+import CoinInfo from './CoinInfo';
 
 interface CoinProps {
   children?: React.ReactNode
@@ -7,25 +9,32 @@ interface CoinProps {
 }
 
 const Coin: React.FC<CoinProps> = ({coin}) => {
+  const navigate = useNavigate();
+  const coinHandler = (coinID: string) => {
+    navigate(`/${coinID}`);
+  }
+
   return (
-    <div className='coin'>
-      <img className="coin__image" src={examplePath} alt="" />
-      <div className='coin__text'>
-        <h3 className="coin__title">{coin.name}</h3>
-        <p className="coin__subtitle">
-          $ {Math.round(Number(coin.priceUsd)*1000)/1000}
-        </p>
+    <>
+      <div className='coin' onClick={() => coinHandler(coin.id)}>
+        <img className="coin__image" src={examplePath} alt="" />
+        <div className='coin__text'>
+          <h3 className="coin__title">{coin.name}</h3>
+          <p className="coin__subtitle">
+            $ {Math.round(Number(coin.priceUsd)*1000)/1000}
+          </p>
+        </div>
+        <div className='coin__text coin__text_rank'>
+          <h3 className="coin__title">rank</h3>
+          <p className="coin__subtitle">
+            {coin.rank}
+          </p>
+        </div>
+        <svg className="coin__angel" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="white" fillRule="evenodd" clipRule="evenodd">
+          <path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"/>
+        </svg>
       </div>
-      <div className='coin__text coin__text_rank'>
-        <h3 className="coin__title">rank</h3>
-        <p className="coin__subtitle">
-          {coin.rank}
-        </p>
-      </div>
-      <svg className="coin__angel" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="white" fillRule="evenodd" clipRule="evenodd">
-        <path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"/>
-      </svg>
-    </div>
+    </>
   );
 };
 
