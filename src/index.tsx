1c89e413b0f42ee761686f47ad92b8b2ef761e35
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-10-02 15:43:41 +0300

    browserRouter => hashRouter for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import './index.sass';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
